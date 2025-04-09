3fd85cb10999c165e4e50c71f505569b82d4ff6b
Tiago de Paula <tiagodepalves@gmail.com>
2025-04-09 04:34:48 -0300

    build(db): fix debug build of sqlite3

---
+++ src/defines.h
@@ -0,0 +1,41 @@
+#ifndef SRC_DEFINES_H
+/** Utility macros. */
+#define SRC_DEFINES_H
+
+#if defined(__clang__)
+/**
+ * Indicates that the pointer is possibly `NULL`.
+ * @see https://clang.llvm.org/docs/AttributeReference.html#nullable
+ */
+#    define NULLABLE _Nullable
+/**
+ * Indicates that the pointer should never be `NULL`.
+ * @see https://clang.llvm.org/docs/AttributeReference.html#nonnull
+ */
+#    define NONNULL _Nonnull
+#else
+// These pointer modifiers are specific to Clang, and are left as comments for readers on GCC.
+#    define NULLABLE
+#    define NONNULL
+#endif
+
+/**
+ * Marker for a branch that should be taken often or should be optimized for. Usually the "happy" case.
+ * Probability is assumed to be 90%.
+ */
+#define likely(x) (__builtin_expect((x), 1))
+/**
+ * Marker for a branch that should be taken rarely or should be optimized against. Usually the error path.
+ * Probability is assumed to be 10%.
+ */
+#define unlikely(x) (__builtin_expect((x), 0))
+
+// https://gcc.gnu.org/onlinedocs/gcc-4.8.5/cpp/Stringification.html
+#define _STR_(X) #X
+/** Stringification macro. */
+#define STR(X) _STR_(X)
+
+/** Happy result, no errors. */
+#define OK 0
+
+#endif  // SRC_DEFINES_H
