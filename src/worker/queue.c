d2f0ce3dd01c4b8bf8985cdb2b727b9d26ada374
Tiago de Paula <tiagodepalves@gmail.com>
2025-04-19 23:35:15 -0300

    feat(queue): concurrent work queue implementation

---
+++ src/worker/queue.c
@@ -0,0 +1,340 @@
+#include <assert.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdalign.h>
+#include <stdatomic.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <bits/pthreadtypes.h>
+#include <bits/time.h>
+#include <immintrin.h>
+#include <pthread.h>
+
+#include "../defines.h"
+#include "./queue.h"
+
+// For safety, simplicity and performance: WORK_QUEUE_CAPACITY must be a divisor of `UINT_MAX + 1`, so it wraps
+// correctly on the UINT_MAX to 0 boundary.
+static_assert(0 < WORK_QUEUE_CAPACITY && WORK_QUEUE_CAPACITY < UINT_MAX);
+static_assert(UINT_MAX % WORK_QUEUE_CAPACITY == WORK_QUEUE_CAPACITY - 1);
+
+/**
+ * Assumed size for the cache line.
+ *
+ * Used to separate atomic variables from synchronization variables, that are used in different code paths.
+ */
+static const constexpr size_t CACHE_LINE_SIZE = 64;
+
+/**
+ * The actual work queue.
+ *
+ * Implemented as a ring buffer, using atomics where possible and synchronization locks otherwise.
+ */
+struct [[gnu::aligned(CACHE_LINE_SIZE)]] work_queue {
+    /** Slow-path synchronisation, mostly isolated on its own cache line. */
+    alignas(CACHE_LINE_SIZE) struct {
+        /**
+         * Signalled on each push, to wake worker threads.
+         */
+        pthread_cond_t item_added_cond;
+        /**
+         * Guards the `item_added_cond`.
+         */
+        pthread_mutex_t item_added_mtx;
+    } cold;
+    /* Ring storage */
+    /**
+     * The ring buffer, limited to `WORK_QUEUE_CAPACITY` items of `work_item`.
+     *
+     * Might share a bit of the cache with other variables, but most of it is on its own cache. This also serves as a
+     * separation between the cache lines for synchronization variables above and the atomics below.
+     */
+    work_item buf[WORK_QUEUE_CAPACITY];
+    /**
+     * Next ticket for consumers to pop. Not capped to `WORK_QUEUE_CAPACITY`.
+     */
+    atomic_uint_fast64_t head;
+    /**
+     * Next ticket for producers to push. Not capped to `WORK_QUEUE_CAPACITY`.
+     */
+    atomic_uint_fast64_t tail;
+};
+
+// We need to ensure the atomic variables are on the same cache line.
+#define cache_line_of(offset)            ((offset) / CACHE_LINE_SIZE)
+#define first_cache_line_of(type, field) cache_line_of(offsetof(type, field))
+#define last_cache_line_of(type, field)  cache_line_of(offsetof(type, field) + sizeof(((type) {}).field))
+static_assert(
+    first_cache_line_of(workq_t, head) == last_cache_line_of(workq_t, tail),
+    "Both atomics should be in the same cache line, because they have true sharing."
+);
+// And ensure they don't interfere with outside cache lines.
+static_assert(sizeof(workq_t) % CACHE_LINE_SIZE == 0, "The shared variables should not interfere with outside cache.");
+
+[[nodiscard("mutex unitialized on false"), gnu::regcall, gnu::nonnull(1)]]
+/** Initialize the mutex with custom attributes. */
+static bool workq_mutex_init(pthread_mutex_t *NONNULL mutex) {
+    pthread_mutexattr_t attr;
+    int rv = pthread_mutexattr_init(&attr);
+    if unlikely (rv != 0) {
+        return false;
+    }
+
+    const int rvs[] = {
+        // Single process only.
+        pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_PRIVATE),
+        // We don't use priorities.
+        pthread_mutexattr_setprotocol(&attr, PTHREAD_PRIO_NONE),
+        // Assume threads can die at any time.
+        pthread_mutexattr_setrobust(&attr, PTHREAD_MUTEX_ROBUST),
+#ifdef DEBUG  // On debug, enable error checking.
+        pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK),
+#else  // On release, use bare threads.
+        pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_NORMAL),
+#endif
+    };
+    for (size_t i = 0; i < sizeof(rvs) / sizeof(int); i++) {
+        if unlikely (rvs[i] != 0) {
+            pthread_mutexattr_destroy(&attr);
+            return false;
+        }
+    }
+
+    rv = pthread_mutex_init(mutex, &attr);
+    pthread_mutexattr_destroy(&attr);
+    return likely(rv == 0);
+}
+
+[[nodiscard("mutex unitialized on false"), gnu::regcall, gnu::nonnull(1)]]
+/** Initialize the condition variables with custom attributes. */
+static bool workq_cond_init(pthread_cond_t *NONNULL cond) {
+    pthread_condattr_t attr;
+    int rv = pthread_condattr_init(&attr);
+    if unlikely (rv != 0) {
+        return false;
+    }
+
+    const int rvs[] = {
+        // Single process only.
+        pthread_condattr_setpshared(&attr, PTHREAD_PROCESS_PRIVATE),
+        // Use low resolution (1 ms to 10 ms) clock.
+        pthread_condattr_setclock(&attr, CLOCK_MONOTONIC_COARSE),
+    };
+    for (size_t i = 0; i < sizeof(rvs) / sizeof(int); i++) {
+        if unlikely (rvs[i] != 0) {
+            pthread_condattr_destroy(&attr);
+            return false;
+        }
+    }
+
+    rv = pthread_cond_init(cond, &attr);
+    pthread_condattr_destroy(&attr);
+    return likely(rv == 0);
+}
+
+/** Allocate memory for the work queue and initialize its synchronization variables. */
+workq_t *NULLABLE workq_create(void) {
+    workq_t *q = aligned_alloc(CACHE_LINE_SIZE, sizeof(struct work_queue));
+    if unlikely (q == NULL) {
+        return NULL;
+    }
+    workq_t *NONNULL queue = get_aligned(workq_t, q);
+    memset(queue, 0, sizeof(struct work_queue));
+
+    bool ok = workq_mutex_init(&(queue->cold.item_added_mtx));
+    if unlikely (!ok) {
+        free(queue);
+        return NULL;
+    }
+
+    ok = workq_cond_init(&(queue->cold.item_added_cond));
+    if unlikely (!ok) {
+        pthread_mutex_destroy(&(queue->cold.item_added_mtx));
+        free(queue);
+        return NULL;
+    }
+
+    atomic_init(&(queue->head), 0);
+    atomic_init(&(queue->tail), 0);
+    return queue;
+}
+
+/** Deallocates memory for the work queue and destroy its synchronization variables. */
+bool workq_destroy(workq_t *NONNULL queue) {
+    if unlikely (queue == NULL) {
+        return false;
+    }
+
+    int rv0 = pthread_cond_destroy(&(queue->cold.item_added_cond));
+    int rv1 = pthread_mutex_destroy(&(queue->cold.item_added_mtx));
+    memset(queue, 0, sizeof(struct work_queue));
+    free(queue);
+
+    return likely(rv0 == 0 && rv1 == 0);
+}
+
+[[nodiscard("lock may fail"), gnu::regcall, gnu::nonnull(1)]]
+/**
+ * Lock a mutex and ensure consistency if last owner died.
+ *
+ * Returns false on errors.
+ */
+static bool workq_mutex_lock(workq_t *NONNULL queue) {
+    int rv = pthread_mutex_lock(&(queue->cold.item_added_mtx));
+    if unlikely (rv == EOWNERDEAD) {
+        rv = pthread_mutex_consistent(&(queue->cold.item_added_mtx));
+    }
+    return likely(rv == 0);
+}
+
+[[gnu::regcall, gnu::nonnull(1)]]
+/**
+ * Signal one worker thread that a new item was added, so it may starting working on that.
+ *
+ * Returns false on errors.
+ */
+static bool workq_signal_item_added(workq_t *NONNULL queue) {
+    bool ok = workq_mutex_lock(queue);
+    if unlikely (!ok) {
+        return false;
+    }
+
+    int rv0 = pthread_cond_signal(&(queue->cold.item_added_cond));
+    int rv1 = pthread_mutex_unlock(&(queue->cold.item_added_mtx));
+    return likely(rv0 == 0 && rv1 == 0);
+}
+
+[[nodiscard("useless call if discarded"), gnu::regcall, gnu::const]]
+/**
+ * Convert `head` and `tail` to valid ring buffer indexes.
+ */
+static inline size_t idx(uint_fast64_t ticket) {
+    return (size_t) (ticket % WORK_QUEUE_CAPACITY);
+}
+
+[[nodiscard("useless call if discarded"), gnu::regcall, gnu::const]]
+static inline int_fast64_t workq_size(uint_fast64_t head, uint_fast64_t tail) {
+    const uint_fast64_t diff = tail - head;
+    if likely (diff <= INT_FAST64_MAX) {
+        return (int_fast64_t) diff;
+    }
+
+    static_assert(((int_fast64_t) -1) == ~((int_fast64_t) 0), "Two's complement representation required");
+    static_assert(sizeof(int_fast64_t) == sizeof(uint_fast64_t), "Input and output types must have the same size");
+    return -(int_fast64_t) (~diff + 1);
+}
+
+/** Mostly lock-free push. Returns `false` on full. */
+bool workq_push(workq_t *NONNULL queue, work_item item) {
+    // we just use head to ensure the queue is not full, so we don't need the latest value
+    uint_fast64_t head = atomic_load_explicit(&(queue->head), memory_order_relaxed);
+    // assuming push is called by only one thread, we already have the latest tail
+    uint_fast64_t tail = atomic_load_explicit(&(queue->tail), memory_order_relaxed);
+
+    if unlikely (workq_size(head, tail) >= WORK_QUEUE_CAPACITY) {
+        // if the queue seems full, then we load the latest head to ensure that is the case before dropping items
+        head = atomic_load_explicit(&(queue->head), memory_order_acquire);
+        if unlikely (workq_size(head, tail) >= WORK_QUEUE_CAPACITY) {
+            assert(workq_size(head, tail) == WORK_QUEUE_CAPACITY);
+            return false;  // actually full
+        }
+    }
+
+    // WARNING: this is racy, if two threads try to push at the same time, which can't happen in this project.
+    queue->buf[idx(tail)] = item;
+
+    bool ok = atomic_compare_exchange_weak_explicit(
+        &(queue->tail),
+        &tail,
+        tail + 1,
+        memory_order_release,  // release tail, for other threads to see
+        memory_order_relaxed
+    );
+    // this has to be true, otherwise we just overwrote another work_item
+    assert(ok);
+
+    assert(workq_size(head, tail) <= WORK_QUEUE_CAPACITY);
+    return likely(ok) && workq_signal_item_added(queue);
+}
+
+/** Lock-free pop. Returns `false` on empty. */
+bool workq_pop(workq_t *NONNULL queue, work_item *NONNULL item) {
+    while (true) {
+        // we can have an outdated head here, because it will be checked again later and it's wrong, then we
+        // just throw away the wrong value from `buf` and try again
+        uint_fast64_t head = atomic_load_explicit(&(queue->head), memory_order_relaxed);
+        // tail must be the latest, otherwise we could get an underflow on subtraction
+        uint_fast64_t tail = atomic_load_explicit(&(queue->tail), memory_order_relaxed);
+
+        if unlikely (workq_size(head, tail) <= 0) {
+            // queue seems empty, we need to get the latest tail and check again
+            tail = atomic_load_explicit(&(queue->tail), memory_order_acquire);
+            if unlikely (workq_size(head, tail) <= 0) {
+                return false;  // actually empty
+            }
+        }
+
+        // At this point we know `head` was a valid position, but the item may have already been taken,
+        // so we can't use it yet.
+        // Also, this was not overwritten by a push, unless it had already been taken by another thread,
+        work_item target = queue->buf[idx(head)];
+        // because a push neve writes to head.
+        // Then we try and announce we have taken it.
+        bool ok = atomic_compare_exchange_weak_explicit(
+            &(queue->head),
+            &head,
+            head + 1,
+            memory_order_acquire,
+            memory_order_relaxed
+        );
+
+        if likely (ok) {
+            assert(workq_size(head, tail) >= 0);
+            // If we took the position and the item, then the read was valid and not overwritten at that point.
+            // Now, we can use it safely.
+            *item = target;
+            return true;
+        }
+
+        // wait a bit before trying again
+        _mm_pause();
+    }
+}
+
+[[nodiscard("useless call if discarded"), gnu::regcall, gnu::pure, gnu::nonnull(1)]]
+/**
+ * Check if the list is empty.
+ */
+static bool is_empty(const workq_t *NONNULL queue) {
+    // memory_order_acquire is not required while holding a mutex in x86, GCC and Clang actually ignores it here,
+    // but they are required in same architectures like ARM
+    const uint_fast64_t head = atomic_load_explicit(&(queue->head), memory_order_acquire);
+    const uint_fast64_t tail = atomic_load_explicit(&(queue->tail), memory_order_acquire);
+    assume(workq_size(head, tail) <= WORK_QUEUE_CAPACITY);
+    return workq_size(head, tail) <= 0;
+}
+
+/**
+ * Block current thread until there is an item to be taken in the work queue.
+ */
+bool workq_wait_not_empty(workq_t *NONNULL queue) {
+    bool ok = workq_mutex_lock(queue);
+    if unlikely (!ok) {
+        return false;
+    }
+
+    int rv0 = 0;
+    while (unlikely(is_empty(queue))) {
+        rv0 = pthread_cond_wait(&(queue->cold.item_added_cond), &(queue->cold.item_added_mtx));
+        if unlikely (rv0 != 0) {
+            break;
+        }
+    }
+
+    // always unlock the mutex, even if `pthread_cond_wait` failed
+    int rv1 = pthread_mutex_unlock(&(queue->cold.item_added_mtx));
+    return likely(rv0 == 0) && likely(rv1 == 0);
+}
---
+++ src/worker/queue.h
@@ -0,0 +1,68 @@
+#ifndef SCR_WORKER_QUEUE_H
+/** Concurrent work queue. */
+#define SCR_WORKER_QUEUE_H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#include "../defines.h"
+
+#define WORK_QUEUE_CAPACITY 128
+
+/**
+ * An opaque handle to the concurrent work queue.
+ */
+typedef struct work_queue workq_t;
+
+/**
+ * The content of the work queue.
+ *
+ * Currently, a socket file descriptor (`int`).
+ */
+typedef int work_item;
+
+[[nodiscard("might need to destroy queue"), gnu::regcall, gnu::malloc, gnu::cold, gnu::leaf, gnu::nothrow]]
+/**
+ * Allocate memory for the work queue and initialize its synchronization variables.
+ *
+ * Returns the a pointer to the new work queue, or `NULL` on out-of-memory situations or other initialization issues.
+ */
+workq_t *NULLABLE workq_create(void);
+
+[[gnu::regcall, gnu::nonnull(1), gnu::cold, gnu::leaf, gnu::nothrow]]
+/**
+ * Deallocates memory for the work queue and destroy its synchronization variables.
+ *
+ * Returns `true` if destruction was successful, or `false` if the synchronization variables are still in use.
+ */
+bool workq_destroy(workq_t *NONNULL queue);
+
+[[gnu::regcall, gnu::nonnull(1), gnu::leaf, gnu::nothrow]]
+/**
+ * Add an item to the queue and signal other threads about it.
+ *
+ * Warning: not thread safe. Should only be called by the main thread.
+ *
+ * Returns `true` if the item was inserted successfully, or `false` if the queue is full or the mutex lock could not be
+ * acquired.
+ */
+bool workq_push(workq_t *NONNULL queue, work_item item);
+
+[[nodiscard("item will be uninitialized on false"), gnu::regcall, gnu::nonnull(1, 2), gnu::leaf, gnu::nothrow]]
+/**
+ * Remove an item from the queue.
+ *
+ * Returns `true` if an item was removed successfully, or `false` if the queue is empty.
+ */
+bool workq_pop(workq_t *NONNULL queue, work_item *NONNULL item);
+
+[[gnu::regcall, gnu::nonnull(1), gnu::leaf, gnu::nothrow]]
+/**
+ * Block current thread until there is an item to be taken in the work queue.
+ *
+ * Returns `true` if the wait was successfull and the work queue is not empty anymore, or `false` in case of issues
+ * with the synchronization variables.
+ */
+bool workq_wait_not_empty(workq_t *NONNULL queue);
+
+#endif /* SCR_WORKER_QUEUE_H */
